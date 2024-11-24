82d22e52c9d4670f8c1a9cf53c7006c4964de5a9
rosa <natsukiarr@gmail.com>
2024-11-23 18:50:43 -0700

    changes INCOMPLETE

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SemaforosController } from './semaforos/semaforos.controller';
+import { VehiculosController } from './vehiculos/vehiculos.controller';
+import { EstacionCentralService } from './estacion-central/estacion-central.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
-  controllers: [AppController, SemaforosController],
-  providers: [AppService],
+  imports: [ClientsModule.register([
+    {
+      name: 'ActualizarMapa',
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'EstacionCentral_queue',
+        queueOptions: {
+          durable: false
+        }
+      }
+    },
+    {
+      name: 'EststadosSemaforos',
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: ''
+      }
+    }
+  ])],
+  controllers: [AppController, SemaforosController, VehiculosController],
+  providers: [AppService, EstacionCentralService],
 })
-export class AppModule {}
+export class AppModule { }
